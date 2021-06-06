348987ce1fe196d1391aa43c0d70fc406fc8efa3
kennytgw <kenny.tgw@gmail.com>
2021-06-06 17:12:10 +0800

    second update

--- datadiy.py
+++ datadiy.py
@@ -4,6 +4,7 @@ import numpy as np
 import time
 import os 
 import base64
+from pandas.api.types import is_numeric_dtype
 
 st.set_page_config(layout="wide")
 
@@ -34,10 +35,6 @@ def download_link(object_to_download, download_filename, download_link_text):
 #======================================
 
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
 @st.cache
 
 def load_data(nrows):
@@ -49,28 +46,34 @@ def load_data(nrows):
 
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
+
 # Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache)")
+data_load_state.text("Cache Loaded! (using st.cache)")
 
 
 tcol1, tcol2, tcol3,tcol4 = st.beta_columns((2,2,1,1))
-col1_sub1, col1_sub2, col1_sub3, col1_sub4, col1_sub5, col1_sub6 = st.beta_columns(6)
-bcol1, bcol2, bcol3 = st.beta_columns(3)
+
+
 
 file1 = tcol1.file_uploader('Upload File 1')
 
 if file1:
     df1 = pd.read_csv(file1)
 
+
+    preview_raw_expander_file_1 = tcol1.beta_expander("Show Raw Dataframe")
+    preview_raw_expander_file_1.write(df1)
+    preview_raw_expander_file_1.write(df1.shape)
+
+
     col_list_1 = list()
 
     for c in df1:
         col_list_1.append(c)
 
-    selected_col_df1 = tcol1.multiselect('Select Columns to use [File 1]', col_list_1)
+    choose_columns_expander_file_1 = tcol1.beta_expander("Trimming Dataframe Columns")
 
+    selected_col_df1 = choose_columns_expander_file_1.multiselect('Select columns to be active in [File 1]. If no selection, all columns will be used',col_list_1)
     if selected_col_df1:
         df1_1 = df1[selected_col_df1]
     else:
@@ -79,14 +82,51 @@ if file1:
 
     #=================================
 
-
-
     col_list_2 = list()
 
     for c in df1_1:
         col_list_2.append(c)
 
-    selected_col_df1_1_null = col1_sub1.multiselect('Select Columns to remove NULL [File 1]', col_list_2)
+
+    #=================================
+
+    unique_value_file_1 = tcol1.beta_expander("Show Number of Unique Values")
+
+    selected_col_df1_1_unique = unique_value_file_1.selectbox('Select columns to show total unique value of [File 1]', col_list_2)
+    
+    if selected_col_df1_1_unique:
+        unique_set = set(df1_1[selected_col_df1_1_unique])
+        unique_len = len(unique_set)
+        
+        unique_value_file_1.write('Number of items: ' + str(unique_len))
+        unique_value_file_1.write(unique_set)
+
+    #=================================
+
+    replace_value_file_1 = tcol1.beta_expander("Replace Values")
+
+    selected_col_df1_1_replace = replace_value_file_1.multiselect('Select columns to replace [File 1]', col_list_2)
+    
+    old_value_file_1 = replace_value_file_1.text_input('Old Value [File 1]')
+    new_value_file_1 = replace_value_file_1.text_input('New Value [File 1]')
+
+    if old_value_file_1 != "" and new_value_file_1 != "":
+        if selected_col_df1_1_replace:
+            df1_1[selected_col_df1_1_replace] = df1_1[selected_col_df1_1_replace].replace(old_value_file_1, new_value_file_1)
+            replace_value_file_1.write('Values replaced')
+
+        else:
+            df1_1 = df1_1.replace(old_value_file_1,new_value_file_1)
+            replace_value_file_1.write('Values replaced')
+
+
+    #=================================
+
+
+
+    remove_null_expander_file_1 = tcol1.beta_expander("Removal of NULL values")
+
+    selected_col_df1_1_null = remove_null_expander_file_1.multiselect('Select columns to have rows removed if NULL in [File 1]. If no selection, all columns will be used', col_list_2)
     if selected_col_df1_1_null:
 
         pass
@@ -94,19 +134,21 @@ if file1:
         selected_col_df1_1_null = selected_col_df1
 
 
-    if col1_sub1.checkbox('Remove NaN rows'):
+    if remove_null_expander_file_1.checkbox('Remove NULL rows [File 1]'):
         df1_1 = df1_1.dropna(subset=selected_col_df1_1_null)
 
     #=================================
 
-    selected_col_df1_1_replace = col1_sub2.multiselect('Select Columns to replace NULL values [File 1]', col_list_2)
+    replace_null_expander_file_1 = tcol1.beta_expander("Replace NULL values")
+
+    selected_col_df1_1_replace = replace_null_expander_file_1.multiselect('Select Columns to replace NULL values [File 1]', col_list_2)
 
     if selected_col_df1_1_replace:
         pass
     else:
         selected_col_df1_1_replace = selected_col_df1
 
-    replace_null_f1 = col1_sub2.radio('Replace NULL with [File 1]:', ('No action','0','Mean', 'Median'))
+    replace_null_f1 = replace_null_expander_file_1.radio('Replace NULL with [File 1]:', ('No action','0','Mean', 'Median'))
 
     if replace_null_f1 == 'No action':
         pass
@@ -118,13 +160,29 @@ if file1:
         df1_1[selected_col_df1_1_replace] = df1_1[selected_col_df1_1_replace].fillna(df1_1.median())
 
 
-    bcol1.write(df1_1)
-    bcol1.write(df1_1.shape)
+
+    minmax_expander_file_1 = tcol1.beta_expander("Show Numeric Range")
+
+    selected_col_df1_1_minmax = minmax_expander_file_1.selectbox('Select to explore numeric statistics [File 1]', col_list_2)
+
+    if selected_col_df1_1_minmax and is_numeric_dtype(df1_1[selected_col_df1_1_minmax]):
+
+       minmax_expander_file_1.write('Minimum : ' + str(df1_1[selected_col_df1_1_minmax].min()))
+       minmax_expander_file_1.write('Maximum : ' + str(df1_1[selected_col_df1_1_minmax].max()))
+       minmax_expander_file_1.write('Mean : ' + str(df1_1[selected_col_df1_1_minmax].mean()))
+       minmax_expander_file_1.write('Median : ' + str(df1_1[selected_col_df1_1_minmax].median()))
+       minmax_expander_file_1.bar_chart(df1_1[selected_col_df1_1_minmax])
+  
+    else:
+        minmax_expander_file_1.write('Invalid column (Non numeric)')
 
 
-    if bcol1.button('Download [File 1] as CSV'):
-        tmp_download_link_file_1 = download_link(df1_1, 'file_1.csv', 'Click here to download your data!')
-        st.markdown(tmp_download_link_file_1, unsafe_allow_html=True)
+
+    preview_mod_expander_file_1 = tcol1.beta_expander("Show Modified Dataframe")
+    preview_mod_expander_file_1.write(df1_1)
+    preview_mod_expander_file_1.write(df1_1.shape)
+
+
 
 
     #=================================
@@ -133,23 +191,30 @@ if file1:
 
 ###FILE 2####
 
+
 file2 = tcol2.file_uploader('Upload File 2')
 
 if file2:
     df2 = pd.read_csv(file2)
 
-    col_list_3 = list()
 
+    preview_raw_expander_file_2 = tcol2.beta_expander("Show Raw Dataframe")
+    preview_raw_expander_file_2.write(df2)
+    preview_raw_expander_file_2.write(df2.shape)
+
+
+    col_list_3 = list()
 
     for c in df2:
         col_list_3.append(c)
 
-    selected_col_df2 = tcol2.multiselect('Select Columns to use [File 2]', col_list_3)
+    choose_columns_expander_file_2 = tcol2.beta_expander("Trimming Dataframe Columns")
 
+    selected_col_df2 = choose_columns_expander_file_2.multiselect('Select columns to be active in [File 2]. If no selection, all columns will be used',col_list_3)
     if selected_col_df2:
         df2_1 = df2[selected_col_df2]
     else:
-        selected_col_df2 = col_list_3
+        selected_col_df2 = col_list_1
         df2_1 = df2[selected_col_df2]
 
     #=================================
@@ -159,45 +224,105 @@ if file2:
     for c in df2_1:
         col_list_4.append(c)
 
-    selected_col_df2_1_null = col1_sub3.multiselect('Select Columns to remove NULL [File 2]', col_list_4)
-    if selected_col_df2_1_null:
-        pass
-    else:
-        selected_col_df2_1_null = selected_col_df2
 
-    if col1_sub3.checkbox('Remove NaN rows [File 2]'):
-        df2_1 = df2_1.dropna(subset=selected_col_df2_1_null)
+    #=================================
 
+    unique_value_file_2 = tcol2.beta_expander("Show Number of Unique Values")
+
+    selected_col_df2_1_unique = unique_value_file_2.selectbox('Select columns to show total unique value of [File 2]', col_list_4)
+    
+    if selected_col_df2_1_unique:
+        unique_set = set(df2_1[selected_col_df2_1_unique])
+        unique_len = len(unique_set)
+        
+        unique_value_file_2.write('Number of items: ' + str(unique_len))
+        unique_value_file_2.write(unique_set)
+
+    #=================================
+
+    replace_value_file_2 = tcol2.beta_expander("Replace Values")
+
+    selected_col_df2_1_replace = replace_value_file_2.multiselect('Select columns to replace [File 2]', col_list_4)
+    
+    old_value_file_2 = replace_value_file_2.text_input('Old Value [File 2]')
+    new_value_file_2 = replace_value_file_2.text_input('New Value [File 2]')
+
+    if old_value_file_2 != "" and new_value_file_2 != "":
+        if selected_col_df2_1_replace:
+            df2_1[selected_col_df2_1_replace] = df2_1[selected_col_df2_1_replace].replace(old_value_file_2, new_value_file_2)
+            replace_value_file_2.write('Values replaced')
+
+        else:
+            df2_1 = df2_1.replace(old_value_file_2,new_value_file_2)
+            replace_value_file_2.write('Values replaced')
 
 
     #=================================
 
-	
-    selected_col_df2_1_replace = col1_sub4.multiselect('Select Columns to replace NULL values [File 2]', col_list_4)
+
+
+    remove_null_expander_file_2 = tcol2.beta_expander("Removal of NULL values")
+
+    selected_col_df2_1_null = remove_null_expander_file_2.multiselect('Select columns to have rows removed if NULL in [File 2]. If no selection, all columns will be used', col_list_4)
+    if selected_col_df2_1_null:
+
+        pass
+    else:
+        selected_col_df2_1_null = selected_col_df2
+
+
+    if remove_null_expander_file_2.checkbox('Remove NULL rows [File 2]'):
+        df2_1 = df2_1.dropna(subset=selected_col_df2_1_null)
+
+    #=================================
+
+    replace_null_expander_file_2 = tcol2.beta_expander("Replace NULL values")
+
+    selected_col_df2_1_replace = replace_null_expander_file_2.multiselect('Select Columns to replace NULL values [File 2]', col_list_4)
 
     if selected_col_df2_1_replace:
         pass
     else:
         selected_col_df2_1_replace = selected_col_df2
 
-    replace_null_f2 = col1_sub4.radio('Replace NULL with [File 2]:', ('No action', '0','Mean', 'Median'))
+    replace_null_f1 = replace_null_expander_file_2.radio('Replace NULL with [File 2]:', ('No action','0','Mean', 'Median'))
 
-    if replace_null_f2 == 'No action':
+    if replace_null_f1 == 'No action':
         pass
-    if replace_null_f2 == '0':
+    if replace_null_f1 == '0':
         df2_1[selected_col_df2_1_replace] = df2_1[selected_col_df2_1_replace].fillna(0)
-    if replace_null_f2 == 'Mean':
-        df2_1[selected_col_df2_1_replace] = df2_1.fillna(df2_1[selected_col_df2_1_replace].mean())
-    if replace_null_f2 == 'Median':
+    if replace_null_f1 == 'Mean':
+        df2_1[selected_col_df2_1_replace] = df2_1[selected_col_df2_1_replace].fillna(df2_1.mean())
+    if replace_null_f1 == 'Median':
         df2_1[selected_col_df2_1_replace] = df2_1[selected_col_df2_1_replace].fillna(df2_1.median())
 
 
-    bcol2.write(df2_1)
-    bcol2.write(df2_1.shape)
 
-    if bcol2.button('Download [File 2] as CSV'):
-        tmp_download_link_file_2 = download_link(df2_1, 'file_2.csv', 'Click here to download your data!')
-        st.markdown(tmp_download_link_file_2, unsafe_allow_html=True)
+    minmax_expander_file_2 = tcol2.beta_expander("Show Numeric Range")
+
+    selected_col_df2_1_minmax = minmax_expander_file_2.selectbox('Select to explore numeric statistics [File 2]', col_list_2)
+
+    if selected_col_df2_1_minmax and is_numeric_dtype(df2_1[selected_col_df2_1_minmax]):
+
+       minmax_expander_file_2.write('Minimum : ' + str(df2_1[selected_col_df2_1_minmax].min()))
+       minmax_expander_file_2.write('Maximum : ' + str(df2_1[selected_col_df2_1_minmax].max()))
+       minmax_expander_file_2.write('Mean : ' + str(df2_1[selected_col_df2_1_minmax].mean()))
+       minmax_expander_file_2.write('Median : ' + str(df2_1[selected_col_df2_1_minmax].median()))
+       minmax_expander_file_2.bar_chart(df2_1[selected_col_df2_1_minmax])
+  
+    else:
+        minmax_expander_file_2.write('Invalid column (Non numeric)')
+
+
+
+    preview_mod_expander_file_2 = tcol2.beta_expander("Show Modified Dataframe")
+    preview_mod_expander_file_2.write(df2_1)
+    preview_mod_expander_file_2.write(df2_1.shape)
+
+
+
+
+    #=================================
 
 
 
@@ -229,3 +354,6 @@ try:
 
 except:
     pass
+
+
+
